ffbbe01f5ee4fa5a15b1e5b94992c38b59eff133
GramenCeleritas <owelman771@gmail.com>
2023-11-04 02:11:08 +0200

    responsive 300px

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 window.onload = function() { 
   const newYears = "1 January 2024"
+  function countdown() {
+    const newYearDate = new Date(newYears)
+    const currentDate = new Date();
+  
+    const totalSec = (newYearDate - currentDate) / 1000;
+    const days = Math.floor(totalSec/3600/24);
+    const hours = Math.floor(totalSec/3600)%24;
+    const mins = Math.floor(totalSec/60)%60;
+  
+    const secs = Math.floor(totalSec)%60;
+  
+  
+    contentMaker()
+
+    function contentMaker() {
+      daysElement.textContent = days.toString(10);
+      hoursElement.textContent = hours.toString();
+      minsElement.textContent = mins.toString();
+      secsElement.textContent = secs.toString();
+      body.classList.add('visible')
+    }
+    
+  }
+  setInterval(countdown, 1000)
   const body = document.querySelector('body')
   const daysElement = document.getElementsByClassName('days')[0];
   const hoursElement = document.getElementsByClassName('hours')[0];
@@ -13,30 +37,13 @@ window.onload = function() {
 
   const headerText = document.getElementsByClassName('headerText')[0];
   
-  function countdown() {
-    const newYearDate = new Date(newYears)
-    const currentDate = new Date();
-  
-    const totalSec = (newYearDate - currentDate) / 1000;
-    const days = Math.floor(totalSec/3600/24);
-    const hours = Math.floor(totalSec/3600)%24;
-    const mins = Math.floor(totalSec/60)%60;
-  
-    const secs = Math.floor(totalSec)%60;
   
   
-    daysElement.textContent = days.toString(10);
-    hoursElement.textContent = hours.toString();
-    minsElement.textContent = mins.toString();
-    secsElement.textContent = secs.toString();
+  
 
-    body.classList.add('visible')
-  }
-  
-  
   
   /// intialize the countdown
-  setInterval(countdown, 1000)
+  
 
   
 }  
