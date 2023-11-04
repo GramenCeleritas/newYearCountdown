2d0acd3d88025bcc2f2b937ede33c3850dfb7e40
GramenCeleritas <owelman771@gmail.com>
2023-11-04 16:31:32 +0200

    notification and audio by click added

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 window.onload = function() { 
   const newYears = "1 January 2024"
+  
   function countdown() {
     const newYearDate = new Date(newYears)
     const currentDate = new Date();
@@ -11,6 +12,7 @@ window.onload = function() {
   
     const secs = Math.floor(totalSec)%60;
   
+
   
     contentMaker()
 
@@ -20,6 +22,7 @@ window.onload = function() {
       minsElement.textContent = mins.toString();
       secsElement.textContent = secs.toString();
       body.classList.add('visible')
+      
     }
     
   }
@@ -29,12 +32,35 @@ window.onload = function() {
   const hoursElement = document.getElementsByClassName('hours')[0];
   const minsElement = document.getElementsByClassName('mins')[0];
   const secsElement = document.getElementsByClassName('seconds')[0];
-
+  const textForNotif = document.getElementsByClassName('notificationText')[0];
+  const notification = document.getElementsByClassName('notification')[0];
   const audio = document.getElementById('testAudio')
 
- 
-  audio.volume = 0.02;
+  setTimeout(() => {
+    textForNotif.classList.add('visibleElement');
+    notification.classList.add('visibleElementNotification');
+  }, 1200)
 
+  setTimeout(() => {
+    textForNotif.classList.remove('visibleElement');
+    notification.classList.remove('visibleElementNotification');
+  }, 6000)
+  
+  
+
+  window.addEventListener('click', () => {
+    
+    audio.play()
+    console.log('you have clicked')
+    
+  })
+
+  setTimeout(() => {
+
+  },5000)
+
+  audio.volume = 0.02;
+  
   const headerText = document.getElementsByClassName('headerText')[0];
   
   
